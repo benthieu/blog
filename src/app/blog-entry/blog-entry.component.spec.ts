3d6d24d827daa47bf9974a4a8cbbf83e06ded21e
Benjamin Mathieu <mathieu.b93@gmail.com>
2021-02-03 18:54:13 +0100

    updated angular

--- src/app/blog-entry/blog-entry.component.spec.ts
+++ src/app/blog-entry/blog-entry.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BlogEntryComponent } from './blog-entry.component';
 
@@ -6,7 +6,7 @@ describe('BlogEntryComponent', () => {
   let component: BlogEntryComponent;
   let fixture: ComponentFixture<BlogEntryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BlogEntryComponent ]
     })
--- src/app/blog-overview/blog-overview.component.spec.ts
+++ src/app/blog-overview/blog-overview.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BlogOverviewComponent } from './blog-overview.component';
 
@@ -6,7 +6,7 @@ describe('BlogOverviewComponent', () => {
   let component: BlogOverviewComponent;
   let fixture: ComponentFixture<BlogOverviewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BlogOverviewComponent ]
     })
